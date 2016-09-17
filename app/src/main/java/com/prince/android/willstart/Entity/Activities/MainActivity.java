40f3ea50e0b349d16d0ec7a2999b97a009efc605
Prince Bansal <Prince Bansal>
2016-09-17 15:52:17 +0530

    added recycler view abstarct

--- app/src/main/java/com/prince/android/willstart/Entity/Activities/MainActivity.java
+++ app/src/main/java/com/prince/android/willstart/Entity/Activities/MainActivity.java
@@ -2,28 +2,39 @@ package com.prince.android.willstart.Entity.Activities;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.prince.android.willstart.Entity.Instances.SearchResult;
 import com.prince.android.willstart.R;
+
+import java.util.List;
 
 /**
  * Created by Prince Bansal Local on 9/17/2016.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private Toolbar toolbar;
     private SearchManager searchManager;
     private SearchView searchView;
     private String keyword;
+    private RecyclerView recyclerView;
+
+    private List<SearchResult> searchResultList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         if(getIntent().hasExtra("keyword")){
             keyword=getIntent().getStringExtra("keyword");
         }
+        handleIntent(getIntent());
     }
 
     @Override
@@ -60,10 +72,70 @@ public class MainActivity extends AppCompatActivity {
         searchView.setIconifiedByDefault(false);
         searchView.setSearchableInfo(
                 searchManager.getSearchableInfo(getComponentName()));
-
+        searchView.setOnQueryTextListener(this);
         if(searchView!=null&&keyword!=null){
-            searchView.setQuery(keyword,false);
+            searchView.setQuery(keyword,true);
         }
+
         return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+        Log.i(TAG, "onNewIntent: ");
+    }
+
+    private void handleIntent(Intent intent) {
+        Log.i(TAG, "handleIntent: ");
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(this,query,Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "handleIntent: "+query);
+            //use the query to search
+        }
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        showMessage(query);
+        return false;
+    }
+
+    private void showMessage(String query) {
+        Toast.makeText(this,query,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        showMessage(newText);
+        return false;
+    }
+
+    public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.SearchViewHolder>{
+
+
+        @Override
+        public SearchResultsAdapter.SearchViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+
+        }
+
+        @Override
+        public void onBindViewHolder(SearchViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        public class SearchViewHolder extends RecyclerView.ViewHolder{
+
+            public SearchViewHolder(View itemView) {
+                super(itemView);
+            }
+        }
     }
 }
