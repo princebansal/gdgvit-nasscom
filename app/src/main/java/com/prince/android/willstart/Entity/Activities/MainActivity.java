5c31748e6f9cf78cf8d0b1a489d9316b7429161a
Prince Bansal <Prince Bansal>
2016-09-17 18:32:50 +0530

    Added listing recycler view

--- app/src/main/java/com/prince/android/willstart/Entity/Activities/MainActivity.java
+++ app/src/main/java/com/prince/android/willstart/Entity/Activities/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -17,12 +19,15 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 import com.bumptech.glide.Glide;
+import com.prince.android.willstart.Boundary.API.ConnectAPI;
 import com.prince.android.willstart.Entity.Instances.Company;
+import com.prince.android.willstart.Entity.Instances.SearchResult;
 import com.prince.android.willstart.R;
 
 import java.util.ArrayList;
@@ -34,7 +39,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
  * Created by Prince Bansal Local on 9/17/2016.
  */
 
-public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener, ConnectAPI.ServerAuthenticateListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private Toolbar toolbar;
@@ -43,8 +48,13 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
     private String keyword;
     private RecyclerView recyclerView;
     private FloatingActionButton fab;
+    private ProgressBar progressBar;
+    private CoordinatorLayout root;
 
     private List<Company> searchResultList;
+
+    private ConnectAPI connectAPI;
+    private SearchResultsAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,24 +66,38 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
     }
 
     private void init() {
-        toolbar=(Toolbar)findViewById(R.id.toolbar);
-        recyclerView=(RecyclerView)findViewById(R.id.recycler_view);
-        fab=(FloatingActionButton)findViewById(R.id.fab);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        connectAPI = new ConnectAPI();
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        root=(CoordinatorLayout)findViewById(R.id.root);
     }
 
     private void setInit() {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Will Start");
-        if(getIntent().hasExtra("keyword")){
-            keyword=getIntent().getStringExtra("keyword");
+        if (getIntent().hasExtra("keyword")) {
+            keyword = getIntent().getStringExtra("keyword");
         }
         handleIntent(getIntent());
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        connectAPI.setServerAuthenticateListener(this);
+        setProgress(true);
+    }
+
+    private void setProgress(boolean b) {
+        progressBar.setVisibility(b ? View.VISIBLE : View.GONE);
+        recyclerView.setVisibility(b ? View.GONE : View.VISIBLE);
+        fab.setVisibility(b ? View.GONE : View.VISIBLE);
+        Animation hyperspaceJump = AnimationUtils.loadAnimation(this, R.anim.scale);
+        hyperspaceJump.setStartTime(500);
+        fab.startAnimation(hyperspaceJump);
     }
 
     private void setData() {
-        List<String> imageUrls=new ArrayList<>();
-        List<String> services=new ArrayList<>();
+        /*List<String> imageUrls = new ArrayList<>();
+        List<String> services = new ArrayList<>();
         services.add("Online Booking");
         services.add("Mobile App");
         services.add("Home Delivery");
@@ -83,26 +107,28 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         imageUrls.add("https://www.seeklogo.net/wp-content/uploads/2014/12/twitter-logo-vector-download.jpg");
         imageUrls.add("http://www.mobygames.com/images/i/26/09/650109.png");
         imageUrls.add("http://www.jamesgood.co.uk/sites/default/files/Logo-Blog_58.png");
-        List<String> names=new ArrayList<>();
+        List<String> names = new ArrayList<>();
         names.add("Google");
         names.add("Dell");
         names.add("Twitter");
         names.add("EA Sports");
         names.add("Starbucks");
-        List<Company> list=new ArrayList<>();
-        for(int i=0;i<5;i++){
-            Company c=new Company();
+        List<Company> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Company c = new Company();
             c.setPicUrl(imageUrls.get(i));
             c.setName(names.get(i));
             c.setServicesOffered(services);
             list.add(c);
         }
-        searchResultList=list;
-        recyclerView.setAdapter(new SearchResultsAdapter(this));
+        searchResultList = list;
+        recyclerView.setAdapter(new SearchResultsAdapter(this));*/
+        if(searchResultList!=null&&searchResultList.size()>0){
+            adapter=new SearchResultsAdapter(this);
+            recyclerView.setAdapter(adapter);
 
-        Animation hyperspaceJump = AnimationUtils.loadAnimation(this, R.anim.scale);
-        hyperspaceJump.setStartTime(500);
-        fab.startAnimation(hyperspaceJump);    }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -119,9 +145,9 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         searchView.setSearchableInfo(
                 searchManager.getSearchableInfo(getComponentName()));
         searchView.setOnQueryTextListener(this);
-        if(searchView!=null&&keyword!=null){
-            searchView.setQuery(null,false);
-            searchView.setQuery(keyword,false);
+        if (searchView != null && keyword != null) {
+            searchView.setQuery(null, false);
+            searchView.setQuery(keyword, true);
         }
 
         return true;
@@ -137,7 +163,7 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         Log.i(TAG, "handleIntent: ");
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            Log.i(TAG, "handleIntent: "+query);
+            Log.i(TAG, "handleIntent: " + query);
             //use the query to search
         }
     }
@@ -145,11 +171,12 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
     @Override
     public boolean onQueryTextSubmit(String query) {
         showMessage(query);
+        connectAPI.fetchMessages(query);
         return false;
     }
 
     private void showMessage(String query) {
-       // Toast.makeText(this,query,Toast.LENGTH_SHORT).show();
+        // Toast.makeText(this,query,Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -158,19 +185,42 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         return false;
     }
 
-    public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.SearchViewHolder>{
+    @Override
+    public void onRequestInitiated(int code) {
+        setProgress(true);
+    }
+
+    @Override
+    public void onRequestCompleted(int code, List<SearchResult> searchResultList) {
+        setProgress(false);
+        this.searchResultList=searchResultList.get(0).getCompanyList();
+        setData();
+    }
+
+    @Override
+    public void onRequestError(int code, String message) {
+        setError();
+    }
+
+    private void setError() {
+        setProgress(true);
+        progressBar.setVisibility(View.GONE);
+        Snackbar.make(root,"Error occured",Snackbar.LENGTH_SHORT).show();
+    }
+
+    public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.SearchViewHolder> {
 
 
         private final Context context;
 
         public SearchResultsAdapter(Context context) {
-            this.context=context;
+            this.context = context;
         }
 
         @Override
         public SearchResultsAdapter.SearchViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view=getLayoutInflater().inflate(R.layout.row_item,parent,false);
-            SearchViewHolder searchViewHolder=new SearchViewHolder(view);
+            View view = getLayoutInflater().inflate(R.layout.row_item, parent, false);
+            SearchViewHolder searchViewHolder = new SearchViewHolder(view);
             return searchViewHolder;
         }
 
@@ -178,10 +228,10 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         public void onBindViewHolder(SearchViewHolder holder, int position) {
             holder.companyName.setText(searchResultList.get(position).getName());
             Glide.with(context).load(searchResultList.get(position).getPicUrl()).asBitmap().into(holder.companyLogo);
-            if(searchResultList.get(position).getServicesOffered()==null){
+            if (searchResultList.get(position).getServicesOffered() == null) {
                 Log.i(TAG, "onBindViewHolder: null");
             }
-            holder.servicesRecycler.setAdapter(new ServicesAdapter(context,searchResultList.get(position).getServicesOffered()));
+            holder.servicesRecycler.setAdapter(new ServicesAdapter(context, searchResultList.get(position).getServicesOffered()));
         }
 
         @Override
@@ -189,7 +239,7 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
             return searchResultList.size();
         }
 
-        public class SearchViewHolder extends RecyclerView.ViewHolder{
+        public class SearchViewHolder extends RecyclerView.ViewHolder {
 
             private CircleImageView companyLogo;
             private TextView companyName;
@@ -197,35 +247,47 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 
             public SearchViewHolder(View itemView) {
                 super(itemView);
-                companyLogo=(CircleImageView)itemView.findViewById(R.id.companyLogo);
-                companyName=(TextView)itemView.findViewById(R.id.companyName);
-                servicesRecycler=(RecyclerView)itemView.findViewById(R.id.recViewFeatures);
-                servicesRecycler.setLayoutManager(new GridLayoutManager(context,2));
+                companyLogo = (CircleImageView) itemView.findViewById(R.id.companyLogo);
+                companyName = (TextView) itemView.findViewById(R.id.companyName);
+                servicesRecycler = (RecyclerView) itemView.findViewById(R.id.recViewFeatures);
+                servicesRecycler.setLayoutManager(new GridLayoutManager(context, 2));
             }
         }
     }
-    public class ServicesAdapter extends RecyclerView.Adapter<ServicesAdapter.ServiceViewHolder>{
+
+    public class ServicesAdapter extends RecyclerView.Adapter<ServicesAdapter.ServiceViewHolder> {
 
 
         private final Context context;
         private List<String> featuresList;
 
-        public ServicesAdapter(Context context,List<String> list) {
-            this.context=context;
-            featuresList=list;
+        public ServicesAdapter(Context context, List<String> list) {
+            this.context = context;
+            featuresList = list;
         }
 
         @Override
         public ServicesAdapter.ServiceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view=getLayoutInflater().inflate(R.layout.feature_item,parent,false);
-            ServiceViewHolder serviceViewHolder=new ServiceViewHolder(view);
+            View view = getLayoutInflater().inflate(R.layout.feature_item, parent, false);
+            ServiceViewHolder serviceViewHolder = new ServiceViewHolder(view);
 
             return serviceViewHolder;
         }
 
         @Override
         public void onBindViewHolder(ServiceViewHolder holder, int position) {
-            holder.featureName.setText(featuresList.get(position));
+            String feat=featuresList.get(position);
+            String[] arr=feat.split("_");
+            StringBuilder  builder=new StringBuilder();
+            for(int i=0;i<arr.length;i++){
+                if(i!=arr.length-1){
+                    builder.append(String.valueOf(arr[i].charAt(0)).toUpperCase()+arr[i].substring(1)+" ");
+                }else{
+                    builder.append(String.valueOf(arr[i].charAt(0)).toUpperCase()+arr[i].substring(1));
+                }
+            }
+            holder.featureName.setText(builder.toString());
+
         }
 
         @Override
@@ -233,13 +295,13 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
             return featuresList.size();
         }
 
-        public class ServiceViewHolder extends RecyclerView.ViewHolder{
+        public class ServiceViewHolder extends RecyclerView.ViewHolder {
 
             private TextView featureName;
 
             public ServiceViewHolder(View itemView) {
                 super(itemView);
-                featureName=(TextView)itemView.findViewById(R.id.featureName);
+                featureName = (TextView) itemView.findViewById(R.id.featureName);
             }
         }
     }
