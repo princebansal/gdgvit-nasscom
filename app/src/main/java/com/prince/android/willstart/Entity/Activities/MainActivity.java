35cc3a2be2b2af4081469666dd746c4cfc2d9944
Shuvam Ghosh <jhukushu@gmail.com>
2016-09-17 18:13:58 +0530

    NoChange

--- app/src/main/java/com/prince/android/willstart/Entity/Activities/MainActivity.java
+++ app/src/main/java/com/prince/android/willstart/Entity/Activities/MainActivity.java
@@ -123,7 +123,6 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
             searchView.setQuery(null,false);
             searchView.setQuery(keyword,false);
         }
-
         return true;
     }
 
