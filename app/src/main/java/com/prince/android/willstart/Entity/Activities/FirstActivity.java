ccb3b012c633ac4a15a6114a7f8ad89bd78c370a
Prince Bansal <Prince Bansal>
2016-09-17 14:14:57 +0530

    added search

--- app/src/main/java/com/prince/android/willstart/Entity/Activities/FirstActivity.java
+++ app/src/main/java/com/prince/android/willstart/Entity/Activities/FirstActivity.java
@@ -3,9 +3,11 @@ package com.prince.android.willstart.Entity.Activities;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.text.TextUtilsCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,18 +32,23 @@ public class FirstActivity extends AppCompatActivity implements ImageButton.OnCl
 
     @Override
     public void onClick(View view) {
+        Intent intent=new Intent(FirstActivity.this, MainActivity.class);
+        if(TextUtils.isEmpty(et.getText().toString())){
+            et.setError("Please enter something");
+        }else{
+            intent.putExtra("keyword",et.getText().toString());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                //Pair<View,String> text=new Pair<>(findViewById(R.id.goal),getString(R.string.transition_activity_text));
+                //Pair<View,String> bg=new Pair<>(findViewById(R.id.dashboard_button),getString(R.string.transition_activity_bg));
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(FirstActivity.this,findViewById(R.id.keyword),"transitionShow");
 
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            //Pair<View,String> text=new Pair<>(findViewById(R.id.goal),getString(R.string.transition_activity_text));
-            //Pair<View,String> bg=new Pair<>(findViewById(R.id.dashboard_button),getString(R.string.transition_activity_bg));
-            ActivityOptionsCompat options = ActivityOptionsCompat.
-                    makeSceneTransitionAnimation(FirstActivity.this,findViewById(R.id.keyword),"transitionShow");
-
-            startActivity(new Intent(FirstActivity.this, MainActivity.class),options.toBundle());
-        }else {
-            startActivity(new Intent(FirstActivity.this, MainActivity.class));
+                startActivity(intent,options.toBundle());
+            }else {
+                startActivity(intent);
+            }
         }
+
 
 
 
