52efd5fba172c5963268b29a238e025b289d2071
Prince Bansal <Prince Bansal>
2016-09-17 11:45:22 +0530

    Changed version

---
+++ app/src/main/java/com/prince/android/willstart/Entity/Activities/SearchResultsActivity.java
@@ -0,0 +1,18 @@
+package com.prince.android.willstart.Entity.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Prince Bansal Local on 9/17/2016.
+ */
+
+public class SearchResultsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
